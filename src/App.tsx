5fd41995549781fe893665c78254a2d9b6e4a94a
Eduardo Francos Margareto <efmargareto@hotmail.com>
2022-11-24 18:41:40 +0100

    LazyLaud base

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import { Navigation } from './routes/Navigation';
 
 const App = () => {
   return (
-    <div className="App">
-      <Navigation></Navigation>
-    </div>
+    <>
+      <Navigation />
+    </>
   );
 }
 
